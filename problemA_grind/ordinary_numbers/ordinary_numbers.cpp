2718b0522a2b6e8e8ef005a4dfd1687466e65582
R4M-0 <omar.chiboub.w@gmail.com>
2026-01-03 23:17:23 +0100

    problem A grind: ordinary numbers

---
+++ problemA_grind/ordinary_numbers/ordinary_numbers.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define endl '\n'
+#define MA9ROUNA_KADHEBA                                                       \
+    ios_base::sync_with_stdio(false);                                          \
+    cin.tie(NULL);                                                             \
+    cout.tie(NULL);
+
+const int MOD = 1000000007;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+ll gcd(ll a, ll b) {
+    return b == 0 ? a : gcd(b, a % b);
+}
+ll lcm(ll a, ll b) {
+    return a * (b / gcd(a, b));
+}
+ll modpow(ll a, ll e, ll mod = MOD) {
+    ll r = 1;
+    while (e) {
+        if (e & 1)
+            r = (r * a) % mod;
+        a = (a * a) % mod;
+        e >>= 1;
+    }
+    return r;
+}
+
+void solve() {
+	ll n;
+        cin >> n;
+
+        string s = to_string(n);
+        int len = s.size();
+
+        int ans = 9 * (len - 1);
+
+        for (int d = 1; d <= 9; d++) {
+            string t(len, char('0' + d));
+            ll val = stoll(t);
+            if (val <= n) ans++;
+        }
+
+        cout << ans << "\n";
+}
+
+int main() {
+    MA9ROUNA_KADHEBA
+    ll t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+        // cout << '\n';
+    }
+    return 0;
+}
