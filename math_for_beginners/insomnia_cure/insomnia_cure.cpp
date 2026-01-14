3add7f17ac9d1b225d021b94458876e2e1f209d9
R4M-0 <omar.chiboub.w@gmail.com>
2026-01-15 00:41:55 +0100

    math for beginners: insomnia cure

---
+++ math_for_beginners/insomnia_cure/insomnia_cure.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define endl '\n'
+#define MA9ROUNA_KADHEBA                                                       \
+    ios_base::sync_with_stdio(false);                                          \
+    cin.tie(NULL);                                                             \
+    cout.tie(NULL);
+
+const int MOD = 1000000007;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+ll gcd(ll a, ll b) {
+    return b == 0 ? a : gcd(b, a % b);
+}
+ll lcm(ll a, ll b) {
+    return a * (b / gcd(a, b));
+}
+ll modpow(ll a, ll e, ll mod = MOD) {
+    ll r = 1;
+    while (e) {
+        if (e & 1)
+            r = (r * a) % mod;
+        a = (a * a) % mod;
+        e >>= 1;
+    }
+    return r;
+}
+
+void solve() {
+	ll k,l,m,n,d; cin >> k >> l >> m >> n >> d;
+	ll mi=min(k,min(l,min(m,n)));
+	if(mi==1){
+		cout << d << endl;
+		return;
+	}
+	ll ans=d;
+	for(ll i =1; i<=d ; i++){
+		if (i%k==0 || i%l==0 || i%m==0 || i%n==0) ans--;
+	}
+	cout << d-ans << endl;
+}
+
+int main() {
+    MA9ROUNA_KADHEBA
+    ll t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+        // cout << '\n';
+    }
+    return 0;
+}
