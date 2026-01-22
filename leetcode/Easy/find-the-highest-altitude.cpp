9ddba197a64d7ec384255f450bb82ea5dec693a3
R4M-0 <omar.chiboub.w@gmail.com>
2026-01-22 12:09:20 +0100

    leetcode: 1732 find the highest altitude

---
+++ leetcode/Easy/find-the-highest-altitude.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int m=0 ,c=0;
+        for(int i=0;i<gain.size();i++){
+            c+=gain[i];
+            m=max(m,c);
+        }
+        return m;
+        
+    }
+};
