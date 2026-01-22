c188c864472f7aee3f8e6bc61c7f52f274859b19
R4M-0 <omar.chiboub.w@gmail.com>
2026-01-22 12:36:30 +0100

    leetcode: 151 reverse words in a string

---
+++ leetcode/Medium/reverse-words-in-a-string.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        stringstream ss(s);
+        string word;
+        vector<string> words;
+        while (ss >> word) {
+            words.push_back(word);
+        }
+        string rev = "";
+        for (int i = words.size() - 1; i >= 0; i--) {
+            rev += words[i];
+            if (i != 0) rev += " ";
+        }
+
+        return rev;
+    }
+};
