caa474d258e425413cab100135a79173b499fa58
R4M-0 <omar.chiboub.w@gmail.com>
2026-01-22 12:30:53 +0100

    leetcode: 3 longest substring without repeating characters

---
+++ leetcode/Medium/longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char, int> lastSeen;
+        int maxLen = 0;
+        int start = 0; 
+
+        for (int end = 0; end < s.length(); end++) {
+            char current = s[end];
+            if (lastSeen.find(current) != lastSeen.end() && lastSeen[current] >= start) {
+                start = lastSeen[current] + 1;
+            }
+            lastSeen[current] = end;
+
+            maxLen = max(maxLen, end - start + 1);
+        }
+
+        return maxLen;
+    }
+};
