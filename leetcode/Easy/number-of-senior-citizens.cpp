00f7620b41e8169eda8f445dbc5d90385a8696f2
R4M-0 <omar.chiboub.w@gmail.com>
2026-01-22 12:17:10 +0100

    leetcode: 2678 number of senior citizens

---
+++ leetcode/Easy/number-of-senior-citizens.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int count = 0;
+        for(auto x:details){
+            if((x[11]-'0')*10+(x[12]-'0')>60){
+                count++;
+            }
+        }
+        return count;
+    }
+};
