990b7b9f7b3e589b147a6e2ad967d18ba39e13e6
R4M-0 <omar.chiboub.w@gmail.com>
2026-01-24 17:42:58 +0100

    recursion and backtracking: summation

---
+++ recursion_and_backtracking/summation/summation.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define endl '\n'
+#define MA9ROUNA_KADHEBA                                                       \
+    ios_base::sync_with_stdio(false);                                          \
+    cin.tie(NULL);                                                             \
+    cout.tie(NULL);
+
+const int MOD = 1000000007;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+ll gcd(ll a, ll b) {
+    return b == 0 ? a : gcd(b, a % b);
+}
+ll lcm(ll a, ll b) {
+    return a * (b / gcd(a, b));
+}
+ll modpow(ll a, ll e, ll mod = MOD) {
+    ll r = 1;
+    while (e) {
+        if (e & 1)
+            r = (r * a) % mod;
+        a = (a * a) % mod;
+        e >>= 1;
+    }
+    return r;
+}
+
+ll summation(vll v , ll n){
+	if(n==0) return v[0];
+	return v[n]+summation(v,n-1);
+}
+
+void solve() {
+	ll n; cin >> n;
+	vll v(n);
+	for(ll & x : v) cin >> x;
+	cout << summation(v,n-1) << endl;
+}
+
+int main() {
+    MA9ROUNA_KADHEBA
+    ll t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+        // cout << '\n';
+    }
+    return 0;
+}
