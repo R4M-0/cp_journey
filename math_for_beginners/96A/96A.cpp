40d20aee46d4ad7751b7b4e5c4d9b07efcd89d6f
R4M-0 <omar.chiboub.w@gmail.com>
2026-02-03 23:34:25 +0100

    math for beginners: 96A

---
+++ math_for_beginners/96A/96A.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define endl '\n'
+#define MA9ROUNA_KADHEBA                                                       \
+    ios_base::sync_with_stdio(false);                                          \
+    cin.tie(NULL);                                                             \
+    cout.tie(NULL);
+
+const int MOD = 1000000007;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+ll gcd(ll a, ll b) {
+    return b == 0 ? a : gcd(b, a % b);
+}
+ll lcm(ll a, ll b) {
+    return a * (b / gcd(a, b));
+}
+ll modpow(ll a, ll e, ll mod = MOD) {
+    ll r = 1;
+    while (e) {
+        if (e & 1)
+            r = (r * a) % mod;
+        a = (a * a) % mod;
+        e >>= 1;
+    }
+    return r;
+}
+
+void solve() {
+    string s; cin >> s;
+    int c = 1;
+    for (int i = 1; i < s.length(); ++i) {
+        if (s[i] == s[i - 1]) {
+            c++;
+        } else {
+            c = 1;
+        }
+        if (c >= 7) {
+            cout << "YES" << endl;
+            return;
+        }
+    }
+    cout << "NO" << endl;
+}
+int main() {
+    MA9ROUNA_KADHEBA
+    ll t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+        // cout << '\n';
+    }
+    return 0;
+}
