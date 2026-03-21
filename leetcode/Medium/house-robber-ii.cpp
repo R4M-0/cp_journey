7678e0b0d0a7644b0efbaaa522f0e29f982ba24f
R4M-0 <omar.chiboub.w@gmail.com>
2026-03-21 15:08:35 +0100

    LeetCode: House Robber II

---
+++ leetcode/Medium/house-robber-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1) return nums[0];
+        vector<int> dp1(n + 2, 0); 
+        vector<int> dp2(n + 2, 0); 
+        for (int i = n - 1; i >= 1; i--) {
+            dp1[i] = max(nums[i] + dp1[i+2], dp1[i+1]);
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            dp2[i] = max(nums[i] + dp2[i+2], dp2[i+1]);
+        }
+        return max(dp1[1], dp2[0]);
+    }
+};
